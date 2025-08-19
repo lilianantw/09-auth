dde76dab32cf3a24e64f441c07950a96dae2dedd
liliana_toiunda <liliana.ntw@gmail.com>
2025-08-19 19:32:19 +0300

    login, register

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,45 +1,51 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { api } from '../../api';
-import { cookies } from 'next/headers';
-import { parse } from 'cookie';
-import { isAxiosError } from 'axios';
-import { logErrorResponse } from '../../_utils/utils';
+import { NextRequest, NextResponse } from "next/server";
+import { api } from "../../api";
+import { parse } from "cookie";
+import { isAxiosError } from "axios";
+import { logErrorResponse } from "../../_utils/utils";
 
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
+    const apiRes = await api.post("auth/register", body);
 
-    const apiRes = await api.post('auth/register', body);
-
-    const cookieStore = await cookies();
-    const setCookie = apiRes.headers['set-cookie'];
+    const setCookie = apiRes.headers["set-cookie"];
+    const res = NextResponse.json(apiRes.data, { status: apiRes.status });
 
     if (setCookie) {
       const cookieArray = Array.isArray(setCookie) ? setCookie : [setCookie];
       for (const cookieStr of cookieArray) {
         const parsed = parse(cookieStr);
-
-        const options = {
-          expires: parsed.Expires ? new Date(parsed.Expires) : undefined,
-          path: parsed.Path,
-          maxAge: Number(parsed['Max-Age']),
-        };
-        if (parsed.accessToken) cookieStore.set('accessToken', parsed.accessToken, options);
-        if (parsed.refreshToken) cookieStore.set('refreshToken', parsed.refreshToken, options);
+        if (parsed.accessToken) {
+          res.cookies.set("accessToken", parsed.accessToken, {
+            path: "/",
+            httpOnly: true,
+            maxAge: parsed["Max-Age"] ? Number(parsed["Max-Age"]) : undefined,
+          });
+        }
+        if (parsed.refreshToken) {
+          res.cookies.set("refreshToken", parsed.refreshToken, {
+            path: "/",
+            httpOnly: true,
+            maxAge: parsed["Max-Age"] ? Number(parsed["Max-Age"]) : undefined,
+          });
+        }
       }
-      return NextResponse.json(apiRes.data, { status: apiRes.status });
     }
 
-    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    return res;
   } catch (error) {
     if (isAxiosError(error)) {
       logErrorResponse(error.response?.data);
       return NextResponse.json(
         { error: error.message, response: error.response?.data },
-        { status: error.status }
+        { status: error.response?.status || 500 }
       );
     }
     logErrorResponse({ message: (error as Error).message });
-    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 }
